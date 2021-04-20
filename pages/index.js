253fb569168bfef67abdcb9cae9da45f19808670
Mitchell Wright <mitchellbwright@gmail.com>
2021-04-20 00:14:37 -0500

    add sign in with Google

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import { useAuth } from '../lib/auth';
+import { useAuth } from "../lib/auth";
 
 export default function Index() {
   const auth = useAuth();
@@ -6,9 +6,18 @@ export default function Index() {
   return auth.user ? (
     <div>
       <p>Email: {auth.user.email}</p>
+      <p>Name: {auth.user.name}</p>
+      <img src={auth.user.photoUrl} />
       <button onClick={(e) => auth.signout()}>Sign Out</button>
     </div>
   ) : (
-    <button onClick={(e) => auth.signinWithGitHub()}>Sign In</button>
-  )
+    <>
+      <button onClick={(e) => auth.signinWithGitHub()}>
+        Sign In With GitHub
+      </button>
+      <button onClick={(e) => auth.signinWithGoogle()}>
+        Sign In With Google
+      </button>
+    </>
+  );
 }
